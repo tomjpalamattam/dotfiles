fe175436f793a1cb5fd2c232f858c80ff3634d07
tomjpalamattam <32675052+tomjpalamattam@users.noreply.github.com>
2024-10-23 14:45:53 +0200

    Delete .config directory

--- .config/hypr/scripts/clipboard-hypr.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-theme="style_clip_hypr"
-export LANG=en_US.UTF-8
-dir="$HOME/.config/rofi/launchers/text"
-styles=($(ls -p --hide="colors.rasi" $dir/styles))
-color="${styles[$(( $RANDOM % 10 ))]}"
-cliphist list | rofi -dmenu -theme $dir/"$theme" | cliphist decode | wl-copy 
-
